0e47c0be2d117bc395be71fbe52c386024c77cd0
Nemironov <nmironov25@seaburyhall.org>
2024-09-20 20:46:24 -1000

    created FrequentWordsWithMismatches but something is up with logic

--- approxpattern.cpp
+++ approxpattern.cpp
@@ -15,16 +15,6 @@ int HammingDistance(string dna1, string dna2)
     return hammingdistance;
 }
 
-void getdna( string dataset )
-{
-    ifstream file;
-    string dna1;
-    string dna2;
-    file.open("C:/Users/Nikolai Mironov/Downloads/dataset_30278_3 (5).txt");
-    while(file >> dna1 >> dna2)
-    cout << HammingDistance(dna1, dna2) << endl;
-} 
-
 string opp(string pattern)
 {
     string reversepattern;
@@ -37,7 +27,17 @@ string opp(string pattern)
     return reversepattern;
 
 }
-
+pair<string, int> mostfrequent(unordered_map<string, int> freqmap)
+{
+    pair<string, int> themax;
+    for (pair<string, int> m : freqmap ) {
+        if (themax.second) {
+            if (m.second > themax.second) themax = m;
+        }
+        else themax = m;
+    }
+    return themax;
+}
 vector<int> approxpatterncount(string pattern, string dna, int d) {
     vector<int> matches;
     for (int i = 0; i<dna.length() - pattern.length() + 1; i++) {
@@ -56,62 +56,60 @@ string suffix(string pattern)
 }
 vector<string> Neighbors(string pattern, int d)
 {
-    cout << pattern << endl;
     if (d == 0) {
     return vector<string> {pattern};
     }
-    if (pattern.length() == 1) return {"A", "C", "G", "T"};
+    if (pattern.length() == 1) return {"A", "C", "G", "T"}; {
     vector<string> Neighborhood;
     vector<string> SuffixNeighbors = Neighbors(suffix(pattern), d);
     for (string Text : SuffixNeighbors){
-        if (HammingDistance(suffix(pattern), Text) < d) {
+        if (HammingDistance(suffix(pattern), Text) == d) {
             vector<string> nucs =  {"A", "C", "G", "T"};
             for (string nuc : nucs) {
-                cout << "text: " << Text << endl;
-                cout << "nuc: " << nuc << endl;
-                cout << "added nuc:" << nuc + Text << endl; 
-                Neighborhood.push_back(nuc + Text);
+                if (nuc != Text) {
+                    Neighborhood.push_back(nuc + Text);
+                }
+
             }
         }
         else {
-            cout << pattern.substr(0, 1) << ", " << Text << ", " << pattern.substr(0, 1) + Text << endl;
             Neighborhood.push_back(pattern.substr(0, 1) + Text);
         }     
     }
-    cout << "pattern: " << pattern << endl;
     return Neighborhood;
+    }
 }
-// vector<string> FrequentWordsWithMismatches(string dna, int k, int d) {
-//     vector<string> patterns;
-//     unordered_map<string, int>;
-//     int n = dna.length();
-//     for (int i = 0; i <= n - k; i++) {
-//         string pattern = dna.substr(i, k);
-//         vector<string> neighborhood = Neighbors(pattern, d)
-//         for (int j = 0; j<= neighborhood.length - 1; j++)
-//             string neighbor = neighborhood[j];
-//             if freqMap[neighbor] doesnt exist
-//                 freqMap[neighbor] ← 1
-//             else
-//                 freqMap[neighbor] ← freqMap[neighbor] + 1
-//     }
-//     m = MaxMap(freqMap)
-//     for every key pattern in freqMap
-//         if freqMap[pattern] = m
-//             append pattern to patterns
-//     return patterns
-// }
+vector<string> FrequentWordsWithMismatches(string dna, int k, int d)
+{
+    vector<string> patterns;
+    unordered_map<string, int> freqMap;
+    int n = dna.length();
+    for (int i = 0; i <= n - k; i++) {
+        string pattern = dna.substr(i, k);
+        vector<string> neighborhood = Neighbors(pattern, d);
+        for (int j = 0; j<= neighborhood.size() - 1; j++) {
+            string neighbor = neighborhood[j];
+            if (freqMap[neighbor] != freqMap[neighbor]) freqMap[neighbor] = 1;
+            else freqMap[neighbor] = freqMap[neighbor] + 1;
+        }
+    }
+    pair<string, int> m = mostfrequent(freqMap);
+    for (auto pattern : freqMap){
+        if (pattern.second == m.second) patterns.push_back(pattern.first);
+    return patterns;
+}
 int main() {
     ifstream file;
     string pattern;
-    string dna;
+    int k;
     int d;
-    vector<int> matches;
-    file.open("C:/Users/Nikolai Mironov/Downloads/dataset_30282_4.txt");
-    while(file >> pattern >> d)
-    pattern = "ACG";
+    file.open("C:/Users/Nikolai Mironov/Downloads/dataset_30278_9.txt");
+    while(file >> pattern >> k >> d);
+    pattern = "ACGTTGCATGTCGCATGATGCATGAGAGCT";
+    k = 4;
     d = 1;
-    cout << pattern << d << " ";
-    for (string neighbor : Neighbors(pattern, d)) cout << neighbor << endl;
+    for (string bob : FrequentWordsWithMismatches(pattern, k, d)) {
+        cout << bob << " ";
+    }
     return 0;
 }
