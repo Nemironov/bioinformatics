50a5d0a23d9d0caef15054aede5b53f9645c06b8
Nemironov <nmironov25@seaburyhall.org>
2024-12-25 17:17:57 -1000

    Initial Commit

---
+++ Bioinformatics/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Bioinformatics
+//
+//  Created by Nikolai Mironov on 12/25/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
